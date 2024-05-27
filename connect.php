50cec29cb7eac92ab0c7532f83c123ce0ff32905
Lenny <Lennard.Stohwasser@gmail.com>
2024-05-27 20:03:37 +0200

    Admin Sicht

---
+++ connect.php
@@ -0,0 +1,18 @@
+<?php
+$frage = $_POST['frage'];
+$antworttyp = $_POST['antworttyp'];
+
+echo"geht net";
+
+$conn = new mysqli('127.0.0.1','root', '123', 'itprojekt');
+if($conn->connect_error){
+	die('connection Failed : '.$conn->connect_error);
+}else{
+	$stmt = $$conn->prepare("eingeben der Frage(frage, antworttyp)values(?,?)");
+    $stmt->bind_param("ss", $frage, $antworttyp);
+	$stmt->execute();
+	echo"Sucessfull";
+	$stmt->clonse();
+	$conn->clonse();
+}
+?>
