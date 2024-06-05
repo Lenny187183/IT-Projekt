53df0f594ad556294cf96a3b7a18fe5ae3b63657
Lenny <Lennard.Stohwasser@gmail.com>
2024-06-05 22:04:16 +0200

    PW

--- connect.php
+++ connect.php
@@ -4,11 +4,11 @@ $antworttyp = $_POST['antworttyp'];
 
 echo"geht net";
 
-$conn = new mysqli('127.0.0.1','013083erika', '123', 'itprojekt');
+$conn = new mysqli('127.0.0.1','123', '123', 'itprojekt');
 if($conn->connect_error){
 	die('connection Failed : '.$conn->connect_error);
 }else{
-	$stmt = $$conn->prepare("eingeben der Frage(frage, antworttyp)values(?,?)");
+	$stmt = $conn->prepare("INSERT INTO(frage, antworttyp)values(?,?)");
     $stmt->bind_param("ss", $frage, $antworttyp);
 	$stmt->execute();
 	echo"Sucessfull";
