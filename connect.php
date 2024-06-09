41de6085ac8aa4825221b43e71c15909c6b86b04
Lenny <Lennard.Stohwasser@gmail.com>
2024-06-09 16:05:19 +0200

    Connect.php

--- connect.php
+++ connect.php
@@ -2,17 +2,19 @@
 $frage = $_POST['frage'];
 $antworttyp = $_POST['antworttyp'];
 
-echo"geht net";
 
-$conn = new mysqli('127.0.0.1','123', '123', 'itprojekt');
+
+$conn = new mysqli('localhost', 'testserver', '123', 'fragen');
 if($conn->connect_error){
 	die('connection Failed : '.$conn->connect_error);
 }else{
-	$stmt = $conn->prepare("INSERT INTO(frage, antworttyp)values(?,?)");
+	$stmt = $conn->prepare("insert into fragen(frage, antworttyp)
+	values(?,?)");
     $stmt->bind_param("ss", $frage, $antworttyp);
 	$stmt->execute();
 	echo"Sucessfull";
-	$stmt->clonse();
-	$conn->clonse();
+	echo"Die Frage wurde übermittelt";
+	$stmt->close();
+	$conn->close();
 }
 ?>
