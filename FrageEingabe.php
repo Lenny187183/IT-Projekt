e818fbee210056b06141f873e04f971a44d19a5a
Lenny <Lennard.Stohwasser@gmail.com>
2024-06-19 10:24:50 +0200

    passt

--- FrageEingabe.php
+++ FrageEingabe.php
@@ -1,6 +1,10 @@
 <?php
+
+
 $frage = $_POST['frage'];
 $antworttyp = $_POST['antworttyp'];
+$ueberschrift = $_POST['Überschrift'];
+$antwortmoeglichkeit = $_POST['Antwortmöglichkeit'];
 
 
 
@@ -8,9 +12,9 @@ $conn = new mysqli('localhost', 'testserver', '123', 'fragen');
 if($conn->connect_error){
 	die('connection Failed : '.$conn->connect_error);
 }else{
-	$stmt = $conn->prepare("insert into fragen(frage, antworttyp)
-	values(?,?)");
-    $stmt->bind_param("ss", $frage, $antworttyp);
+	$stmt = $conn->prepare("insert into fragen(frage, antworttyp, Überschrift, Antwortmöglichkeit)
+	values(?,?,?,?)");
+    $stmt->bind_param("ssss", $frage, $antworttyp, $ueberschrift, $antwortmoeglichkeit);
 	$stmt->execute();
 	echo"Sucessfull";
 	echo"Die Frage wurde übermittelt";
